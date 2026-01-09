8e67f1ccaf3caa5e2afd62eacf88db55c7d153f7
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2026-01-09 13:48:25 +0800

    add main to Renderer.cpp and remove main.cpp

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -191,3 +191,14 @@ void Renderer::Shutdown()
     SDL_Quit();
     SDL_Log("[Shutdown] Clean exit");
 }
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+    Renderer renderer{};
+    if (!renderer.Initialize())
+        return 1;
+
+    renderer.Run();
+    renderer.Shutdown();
+    return 0;
+}
